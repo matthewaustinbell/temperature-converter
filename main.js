8027c1faefbf41a1ff3682bab1c5c16b52393880
Matt Bell <matthewaustinbell@gmail.com>
2019-03-18 18:11:46 -0500

    new things

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ console.log('hello');
 
 const toCelsius =  () => {
 
+    
+    if (degree == "C") {
+      x = document.getElementById("c").value * 9 / 5 + 32;
+      document.getElementById("f").value = Math.round(x);
+    } else {
+      x = (document.getElementById("f").value -32) * 5 / 9;
+      document.getElementById("c").value = Math.round(x);
+
 }
 
 const toFahrenheit =  () => {
