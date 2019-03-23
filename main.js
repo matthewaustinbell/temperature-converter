a2bd09a62a2e4e83b2e75ec76d305ea0fbff7213
Matt Bell <matthewaustinbell@gmail.com>
2019-03-22 21:19:04 -0500

    added temp function to js

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-const celciusInput = document.querySelector('#celcius > input'); 
-const fahrenhietInput = document.querySelector ('#fahrenheit > input');
 
-function celciusToFahrenhiet() {
+
+const celciusInput = document.querySelector('#celcius > input'); 
+const fahrenheitInput = document.querySelector ('#fahrenheit > input');
+
+function roundNum(num) {
+  return Math.round(num*100)/100; 
+}
+
+function celciusToFahrenheit() {
   const cTemp = parseFloat(celciusInput.value);
   const fTemp = (cTemp * (9/5)) + 32;
-  fahrenhietInput.value = fTemp;
- 
+  fahrenheitInput.value = roundNum(fTemp);
+  celciusInput.value = roundNum(cTemp);
  }
 
  function fahrenheitToCelcius() {
-   const fTemp = parseFLoat(fahrenhietInput.value);
+   const fTemp = parseFloat(fahrenheitInput.value);
    const cTemp = (fTemp - 32) * (5/9);
-   celciusInput.value = cTemp;
-
+   celciusInput.value = roundNum(cTemp);
+   fahrenheitInput.value = roundNum(fTemp);
  }
 
-celciusInput.addEventListener('input', celciusToFahrenhiet);
 
+
+function main() {
+  celciusInput.addEventListener('input', celciusToFahrenheit);
+  fahrenheitInput.addEventListener('input', fahrenheitToCelcius);
+}
+
+main();
